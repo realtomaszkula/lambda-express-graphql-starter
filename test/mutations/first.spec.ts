044b4c6c982a00e48c82eddb72d0d04075139c76
Tomasz Kula <tomaszkula07@gmail.com>
2017-08-30 18:01:20 +0200

    Add jest

--- test/mutations/first.spec.ts
+++ test/mutations/first.spec.ts
@@ -2,4 +2,8 @@ describe('.env', () => {
   it('loads env before each test', () => {
     expect(process.env.hello).toEqual('world');
   });
+
+  it('imports matchers', () => {
+    expect('that is the question').toBeOrNotToBe();
+  });
 });
--- test/setupJest.ts
+++ test/setupJest.ts
@@ -1,4 +1,9 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
+import { matchers } from './matchers';
 
 dotenv.config({ path: path.join(__dirname, '.env') });
+
+beforeEach(() => {
+  expect.extend(matchers);
+});
