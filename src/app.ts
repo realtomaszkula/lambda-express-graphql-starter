b4d3f60330438a8379c39d60758213c2883e4f49
Tomasz Kula <tomaszkula07@gmail.com>
2017-08-30 19:05:27 +0200

    Add cors and body parser

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 import * as graphqlHTTP from 'express-graphql';
 
 import { createLoaders, db, Database, DataLoaders } from './db';
@@ -11,6 +13,9 @@ export interface ApiContext {
 
 export const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/languages', (req, res) => {
 
   const loaders = createLoaders(db);
